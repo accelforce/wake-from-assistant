4572ce5a4a6918a0a8d0d2602d4452de6d5b4301
kyori19 <kyori@accelf.net>
2022-07-02 20:38:50 +0000

    Increase timeout of ping

--- src/main/kotlin/net/accelf/wakefromassistant/helpers/InetAddress.kt
+++ src/main/kotlin/net/accelf/wakefromassistant/helpers/InetAddress.kt
@@ -3,4 +3,4 @@ package net.accelf.wakefromassistant.helpers
 import java.net.InetAddress
 
 fun InetAddress.isReachable(): Result<Boolean> =
-    runCatching { isReachable(500) }
+    runCatching { isReachable(5000) }
